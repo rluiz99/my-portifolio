08a425480a0a7652164b9b4a0de4de05556f8fee
Ronaldo Luiz <ronaldoluiz747@gmail.com>
2023-01-12 13:34:14 -0300

    layout correction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,35 +5,26 @@ import logo from '../assets/logo.svg'
 export function Header() {
     return(
 
-        <header className={styles.header}>
-            <div className={styles.author}>
+        <div className={styles.profile}>
+            {/* <div className={styles.author}> */}
                 <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/121819777?v=4" />
-                <div className={styles.author2} >
-                    <img className={styles.logo} src={logo} alt="Logotipo" />
-                    <strong>onaldo Luiz </strong>
+                <div className={styles.info} >
+                    <span>
+                        <img className={styles.logo} src={logo} alt="Logotipo" />
+                        <strong>onaldo Luiz </strong>
+                    </span>
+                    <span>
+                        <strong>Desenvolvedor Fullstack Jr.</strong>
+                        {/* <ArrowsClockwise size={20} weight="bold" /> */}
+                    </span>
+                    <div className={styles.stack}> 
+                        <span>HTML</span>
+                        <span>CSS</span>
+                        <span>Node</span>
+                        <span>SQL</span>
+                        <span>Javascript</span>
+                    </div>
                 </div>
-
-                <div className={styles.description}>
-                    <strong>Desenvolvedor Fullstack Jr.</strong>
-                    <ArrowsClockwise size={20} weight="bold" />
-                </div>
-
-                <div className={styles.content}>
-                    {/* <p> Descover👋</p>
-                    <p> About Me 🚀</p>
-                    <p>{' '}<a href="">👉 jane.design/doctorcare </a></p> */}
-                    <p> 
-                        <a href=""> #HTML</a>{' '}
-                        <a href=""> #CSS</a>{' '}
-                        <a href=""> #Node</a>{' '}
-                        <a href=""> #SQL</a>{' '}
-                        <a href=""> #Javascript</a>
-                    </p>
-               
-                </div>
-            </div>
-
-
-        </header>
+        </div>
     );
 }
